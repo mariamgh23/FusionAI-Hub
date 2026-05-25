a3f91fd8a1f51b53406b32422628418eedf961a8
Mariam Ghareeb <130003742+mariamgh23@users.noreply.github.com>
2026-05-25 15:56:30 +0300

    Add VPC and subnet resources for Huawei Cloud

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,11 @@
+resource "huaweicloud_vpc" "fusion_vpc" {
+  name = "fusion-vpc"
+  cidr = "192.168.0.0/16"
+}
 
+resource "huaweicloud_vpc_subnet" "fusion_subnet" {
+  name       = "fusion-subnet"
+  cidr       = "192.168.1.0/24"
+  gateway_ip = "192.168.1.1"
+  vpc_id     = huaweicloud_vpc.fusion_vpc.id
+}
