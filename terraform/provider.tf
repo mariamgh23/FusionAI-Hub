815ee1af813c230e6646f06d3720c1693fe65bcc
Mariam Ghareeb <130003742+mariamgh23@users.noreply.github.com>
2026-05-25 15:55:48 +0300

    Add Huaweicloud provider configuration

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,1 +1,14 @@
+terraform {
+  required_providers {
+    huaweicloud = {
+      source  = "huaweicloud/huaweicloud"
+      version = ">= 1.40.0"
+    }
+  }
+}
 
+provider "huaweicloud" {
+  region     = var.region
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
