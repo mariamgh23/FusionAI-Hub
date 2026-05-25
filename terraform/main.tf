de302942be8917e3d52ec280a7ec7d07287de229
Mariam Ghareeb <130003742+mariamgh23@users.noreply.github.com>
2026-05-25 16:17:03 +0300

    Add compute instance resource for fusion server

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,3 +9,13 @@ resource "huaweicloud_vpc_subnet" "fusion_subnet" {
   gateway_ip = "192.168.1.1"
   vpc_id     = huaweicloud_vpc.fusion_vpc.id
 }
+resource "huaweicloud_compute_instance" "fusion_server" {
+  name            = "fusion-server"
+  image_name      = "Ubuntu 22.04 server 64bit"
+  flavor_name     = "s6.small.1"
+  security_groups = ["default"]
+
+  network {
+    uuid = huaweicloud_vpc_subnet.fusion_subnet.id
+  }
+}
